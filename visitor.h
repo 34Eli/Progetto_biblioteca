54fab20c467af477fd358a2e7b36ecd567d62541
34Eli <elisa.marchioro04@gmail.com>
2025-08-14 18:19:47 +0200

    Sistemata classe visitor

--- visitor.h
+++ visitor.h
@@ -10,11 +10,11 @@ class Visitor{
 
     public:
         Visitor();
-        virtual void visitFilm(Film* f) = 0;
-        virtual void visitBook(Book* b) = 0;
-        virtual void visitMusic(Music* m) = 0;
-        virtual void visitPhotograph(Photograph* p) = 0;
-        virtual void visitVideogame(Videogame* v) = 0;
+        virtual void visitFilm(Film& f) = 0;
+        virtual void visitBook(Book& b) = 0;
+        virtual void visitMusic(Music& m) = 0;
+        virtual void visitPhotograph(Photograph& p) = 0;
+        virtual void visitVideogame(Videogame& v) = 0;
         virtual ~Visitor() = default;
 };
 
