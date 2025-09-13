c03563ccb0e0cb3b71c3ebae4ff2c848ac41a8e9
34Eli <elisa.marchioro04@gmail.com>
2025-09-13 22:01:11 +0200

    Sistemato xmlreader

--- Sources/Data/XML/xmlreader.cpp
+++ Sources/Data/XML/xmlreader.cpp
@@ -1,7 +1,7 @@
 #include "Sources/Data/XML/xmlreader.h"
 #include <QDebug>
 
-XmlReader::XmlReader() {}
+/*XmlReader::XmlReader() {}
 
 QList<Product*> XmlReader::readAll(const QString &filename) {
     QList<Product*> library;
@@ -41,22 +41,6 @@ QList<Product*> XmlReader::readAll(const QString &filename) {
 
 
 void XmlReader::readProduct(QXmlStreamReader &xml, ProductData& prod) {
-    //ProductData prod;
-
-    /*while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "Product")) {
-        xml.readNext();
-        if (xml.tokenType() == QXmlStreamReader::StartElement) {
-            if (xml.name() == "name") prod.name = xml.readElementText();
-            else if (xml.name() == "description") prod.description = xml.readElementText();
-            else if (xml.name() == "genre") prod.genre = xml.readElementText();
-            else if (xml.name() == "country") prod.country = xml.readElementText();
-            else if (xml.name() == "year_of_publication") prod.year_of_publication = xml.readElementText().toInt();
-            else if (xml.name() == "cost") prod.cost = xml.readElementText().toInt();
-            else if (xml.name() == "stars") prod.stars = xml.readElementText().toInt();
-            else if (xml.name() == "imagePath") prod.imagePath = xml.readElementText();
-        }
-    }
-    return prod;*/
 
     const QString tag = xml.name().toString();
     const QString value = xml.readElementText();
@@ -72,56 +56,19 @@ void XmlReader::readProduct(QXmlStreamReader &xml, ProductData& prod) {
 }
 
 void XmlReader::readDigital(QXmlStreamReader &xml, DigitalData& dig) {
-    //DigitalData dig;
 
-    /*while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "Product")) {
-        xml.readNext();
-        if (xml.tokenType() == QXmlStreamReader::StartElement) {
-            if (xml.name() == "company") dig.company = xml.readElementText();
-        }
-    }
-
-    return dig;*/
     if (xml.name() == "company")
         dig.company = xml.readElementText();
 }
 
 void XmlReader::readPhysical(QXmlStreamReader &xml, PhysicalData& phy) {
-    //PhysicalData phy;
 
-    /*while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "Product")) {
-        xml.readNext();
-        if (xml.tokenType() == QXmlStreamReader::StartElement) {
-            if (xml.name() == "author") phy.author = xml.readElementText();
-        }
-    }
-
-    return phy;*/
     if (xml.name() == "author")
         phy.author = xml.readElementText();
 }
 
 Film* XmlReader::readFilm(QXmlStreamReader &xml) {
-    /*ProductData prod = readProduct(xml);
-    DigitalData dig = readDigital(xml);
 
-    QString director, mainActor;
-    int minutes = 0;
-
-    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "Product")) {
-        xml.readNext();
-        if (xml.tokenType() == QXmlStreamReader::StartElement) {
-            if (xml.name() == "director") director = xml.readElementText();
-            else if (xml.name() == "mainActor") mainActor = xml.readElementText();
-            else if (xml.name() == "minutes") minutes = xml.readElementText().toInt();
-        }
-    }
-
-    return new Film(prod.name.toStdString(), prod.description.toStdString(),
-                    prod.genre.toStdString(), prod.country.toStdString(),
-                    prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
-                    dig.company.toStdString(), director.toStdString(),
-                    mainActor.toStdString(), minutes);*/
     ProductData prod;
     DigitalData dig;
     QString director, mainActor;
@@ -150,26 +97,7 @@ Film* XmlReader::readFilm(QXmlStreamReader &xml) {
 }
 
 Music* XmlReader::readMusic(QXmlStreamReader &xml) {
-    /*ProductData prod = readProduct(xml);
-    DigitalData dig = readDigital(xml);
 
-    QString singer, album;
-    int minutes = 0;
-
-    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "Product")) {
-        xml.readNext();
-        if (xml.tokenType() == QXmlStreamReader::StartElement) {
-            if (xml.name() == "singer") singer = xml.readElementText();
-            else if (xml.name() == "album") album = xml.readElementText();
-            else if (xml.name() == "minutes") minutes = xml.readElementText().toInt();
-        }
-    }
-
-    return new Music(prod.name.toStdString(), prod.description.toStdString(),
-                     prod.genre.toStdString(), prod.country.toStdString(),
-                     prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
-                     dig.company.toStdString(), singer.toStdString(),
-                     album.toStdString(), minutes);*/
     ProductData prod;
     DigitalData dig;
     QString singer, album;
@@ -197,25 +125,7 @@ Music* XmlReader::readMusic(QXmlStreamReader &xml) {
 }
 
 Videogame* XmlReader::readVideogame(QXmlStreamReader &xml) {
-    /*ProductData prod = readProduct(xml);
-    DigitalData dig = readDigital(xml);
 
-    QString platform;
-    bool isMultiplayer = false;
-
-    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "Product")) {
-        xml.readNext();
-        if (xml.tokenType() == QXmlStreamReader::StartElement) {
-            if (xml.name() == "platform") platform = xml.readElementText();
-            else if (xml.name() == "isMultiplayer") isMultiplayer = (xml.readElementText() == "true");
-        }
-    }
-
-    return new Videogame(prod.name.toStdString(), prod.description.toStdString(),
-                         prod.genre.toStdString(), prod.country.toStdString(),
-                         prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
-                         dig.company.toStdString(), platform.toStdString(),
-                         isMultiplayer);*/
     ProductData prod;
     DigitalData dig;
     QString platform;
@@ -242,27 +152,7 @@ Videogame* XmlReader::readVideogame(QXmlStreamReader &xml) {
 }
 
 Book* XmlReader::readBook(QXmlStreamReader &xml) {
-    /*ProductData prod = readProduct(xml);
-    PhysicalData phy = readPhysical(xml);
 
-    int pages = 0;
-    QString publisher;
-    int ISBN = 0;
-
-    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "Product")) {
-        xml.readNext();
-        if (xml.tokenType() == QXmlStreamReader::StartElement) {
-            if (xml.name() == "pages") pages = xml.readElementText().toInt();
-            else if (xml.name() == "publisher") publisher = xml.readElementText();
-            else if (xml.name() == "ISBN") ISBN = xml.readElementText().toInt();
-        }
-    }
-
-    return new Book(prod.name.toStdString(), prod.description.toStdString(),
-                    prod.genre.toStdString(), prod.country.toStdString(),
-                    prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
-                    phy.author.toStdString(), pages,
-                    publisher.toStdString(), ISBN);*/
     ProductData prod;
     PhysicalData phy;
     int pages = 0, ISBN = 0;
@@ -290,25 +180,7 @@ Book* XmlReader::readBook(QXmlStreamReader &xml) {
 }
 
 Photograph* XmlReader::readPhotograph(QXmlStreamReader &xml) {
-    /*ProductData prod = readProduct(xml);
-    PhysicalData phy = readPhysical(xml);
 
-    bool isColourful = false;
-    int length = 0, width = 0;
-
-    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "Product")) {
-        xml.readNext();
-        if (xml.tokenType() == QXmlStreamReader::StartElement) {
-            if (xml.name() == "isColourful") isColourful = (xml.readElementText() == "true");
-            else if (xml.name() == "length") length = xml.readElementText().toInt();
-            else if (xml.name() == "width") width = xml.readElementText().toInt();
-        }
-    }
-
-    return new Photograph(prod.name.toStdString(), prod.description.toStdString(),
-                          prod.genre.toStdString(), prod.country.toStdString(),
-                          prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
-                          phy.author.toStdString(), isColourful, length, width);*/
     ProductData prod;
     PhysicalData phy;
     bool isColourful = false;
@@ -324,6 +196,201 @@ Photograph* XmlReader::readPhotograph(QXmlStreamReader &xml) {
             else if (tag == "isColourful") isColourful = (xml.readElementText() == "true");
             else if (tag == "length") length = xml.readElementText().toInt();
             else if (tag == "width") width = xml.readElementText().toInt();
+            else readProduct(xml, prod);
+        }
+    }
+
+    return new Photograph(prod.name.toStdString(), prod.description.toStdString(),
+                          prod.genre.toStdString(), prod.country.toStdString(),
+                          prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
+                          phy.author.toStdString(), isColourful, length, width);
+}*/
+XmlReader::XmlReader() {}
+
+QList<Product*> XmlReader::readAll(const QString &filename) {
+    QList<Product*> library;
+    QFile file(filename);
+
+    if (!file.open(QIODevice::ReadOnly)) {
+        qWarning() << "Impossibile aprire il file XML";
+        return library;
+    }
+
+    QXmlStreamReader xml(&file);
+
+    while (!xml.atEnd() && !xml.hasError()) {
+        QXmlStreamReader::TokenType token = xml.readNext();
+
+        if (token == QXmlStreamReader::StartElement && xml.name() == QStringLiteral("Product")) {
+            QString type = xml.attributes().value("type").toString();
+
+            Product* product = nullptr;
+
+            if (type == "Film") product = readFilm(xml);
+            else if (type == "Music") product = readMusic(xml);
+            else if (type == "Videogame") product = readVideogame(xml);
+            else if (type == "Book") product = readBook(xml);
+            else if (type == "Photograph") product = readPhotograph(xml);
+            else qWarning() << "Classe non valida:" << type;
+
+            if (product != nullptr) library.append(product);
+        }
+    }
+
+    if (xml.hasError())
+        qWarning() << "Errore parsing XML:" << xml.errorString();
+
+    xml.clear();
+    return library;
+}
+
+void XmlReader::readProduct(QXmlStreamReader &xml, ProductData& prod) {
+    const QString tag = xml.name().toString();
+    const QString value = xml.readElementText();
+
+    if (tag == QStringLiteral("name")) prod.name = value;
+    else if (tag == QStringLiteral("description")) prod.description = value;
+    else if (tag == QStringLiteral("genre")) prod.genre = value;
+    else if (tag == QStringLiteral("country")) prod.country = value;
+    else if (tag == QStringLiteral("year_of_publication")) prod.year_of_publication = value.toInt();
+    else if (tag == QStringLiteral("cost")) prod.cost = value.toInt();
+    else if (tag == QStringLiteral("stars")) prod.stars = value.toInt();
+    else if (tag == QStringLiteral("imagePath")) prod.imagePath = value;
+}
+
+void XmlReader::readDigital(QXmlStreamReader &xml, DigitalData& dig) {
+    if (xml.name() == QStringLiteral("company"))
+        dig.company = xml.readElementText();
+}
+
+void XmlReader::readPhysical(QXmlStreamReader &xml, PhysicalData& phy) {
+    if (xml.name() == QStringLiteral("author"))
+        phy.author = xml.readElementText();
+}
+
+Film* XmlReader::readFilm(QXmlStreamReader &xml) {
+    ProductData prod;
+    DigitalData dig;
+    QString director, mainActor;
+    int minutes = 0;
+
+    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == QStringLiteral("Product"))) {
+        xml.readNext();
+
+        if (xml.tokenType() == QXmlStreamReader::StartElement) {
+            QString tag = xml.name().toString();
+
+            if (tag == QStringLiteral("company")) readDigital(xml, dig);
+            else if (tag == QStringLiteral("director")) director = xml.readElementText();
+            else if (tag == QStringLiteral("mainActor")) mainActor = xml.readElementText();
+            else if (tag == QStringLiteral("minutes")) minutes = xml.readElementText().toInt();
+            else readProduct(xml, prod);
+        }
+    }
+
+    return new Film(prod.name.toStdString(), prod.description.toStdString(),
+                    prod.genre.toStdString(), prod.country.toStdString(),
+                    prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
+                    dig.company.toStdString(), director.toStdString(),
+                    mainActor.toStdString(), minutes);
+}
+
+Music* XmlReader::readMusic(QXmlStreamReader &xml) {
+    ProductData prod;
+    DigitalData dig;
+    QString singer, album;
+    int minutes = 0;
+
+    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == QStringLiteral("Product"))) {
+        xml.readNext();
+
+        if (xml.tokenType() == QXmlStreamReader::StartElement) {
+            QString tag = xml.name().toString();
+
+            if (tag == QStringLiteral("company")) readDigital(xml, dig);
+            else if (tag == QStringLiteral("singer")) singer = xml.readElementText();
+            else if (tag == QStringLiteral("album")) album = xml.readElementText();
+            else if (tag == QStringLiteral("minutes")) minutes = xml.readElementText().toInt();
+            else readProduct(xml, prod);
+        }
+    }
+
+    return new Music(prod.name.toStdString(), prod.description.toStdString(),
+                     prod.genre.toStdString(), prod.country.toStdString(),
+                     prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
+                     dig.company.toStdString(), singer.toStdString(),
+                     album.toStdString(), minutes);
+}
+
+Videogame* XmlReader::readVideogame(QXmlStreamReader &xml) {
+    ProductData prod;
+    DigitalData dig;
+    QString platform;
+    bool isMultiplayer = false;
+
+    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == QStringLiteral("Product"))) {
+        xml.readNext();
+
+        if (xml.tokenType() == QXmlStreamReader::StartElement) {
+            QString tag = xml.name().toString();
+
+            if (tag == QStringLiteral("company")) readDigital(xml, dig);
+            else if (tag == QStringLiteral("platform")) platform = xml.readElementText();
+            else if (tag == QStringLiteral("isMultiplayer")) isMultiplayer = (xml.readElementText() == "true");
+            else readProduct(xml, prod);
+        }
+    }
+
+    return new Videogame(prod.name.toStdString(), prod.description.toStdString(),
+                         prod.genre.toStdString(), prod.country.toStdString(),
+                         prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
+                         dig.company.toStdString(), platform.toStdString(),
+                         isMultiplayer);
+}
+
+Book* XmlReader::readBook(QXmlStreamReader &xml) {
+    ProductData prod;
+    PhysicalData phy;
+    int pages = 0, ISBN = 0;
+    QString publisher;
+
+    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == QStringLiteral("Product"))) {
+        xml.readNext();
+
+        if (xml.tokenType() == QXmlStreamReader::StartElement) {
+            QString tag = xml.name().toString();
+
+            if (tag == QStringLiteral("author")) readPhysical(xml, phy);
+            else if (tag == QStringLiteral("pages")) pages = xml.readElementText().toInt();
+            else if (tag == QStringLiteral("publisher")) publisher = xml.readElementText();
+            else if (tag == QStringLiteral("ISBN")) ISBN = xml.readElementText().toInt();
+            else readProduct(xml, prod);
+        }
+    }
+
+    return new Book(prod.name.toStdString(), prod.description.toStdString(),
+                    prod.genre.toStdString(), prod.country.toStdString(),
+                    prod.year_of_publication, prod.cost, prod.stars, prod.imagePath.toStdString(),
+                    phy.author.toStdString(), pages,
+                    publisher.toStdString(), ISBN);
+}
+
+Photograph* XmlReader::readPhotograph(QXmlStreamReader &xml) {
+    ProductData prod;
+    PhysicalData phy;
+    bool isColourful = false;
+    int length = 0, width = 0;
+
+    while (!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == QStringLiteral("Product"))) {
+        xml.readNext();
+
+        if (xml.tokenType() == QXmlStreamReader::StartElement) {
+            QString tag = xml.name().toString();
+
+            if (tag == QStringLiteral("author")) readPhysical(xml, phy);
+            else if (tag == QStringLiteral("isColourful")) isColourful = (xml.readElementText() == "true");
+            else if (tag == QStringLiteral("length")) length = xml.readElementText().toInt();
+            else if (tag == QStringLiteral("width")) width = xml.readElementText().toInt();
             else readProduct(xml, prod);
         }
     }
